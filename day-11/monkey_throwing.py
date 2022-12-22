f9c9da83e6ea891972a39da8e2de3f521dcf11a4
Sebaa <sebastiankostr@gmail.com>
2022-12-22 18:39:44 +0100

    done day 11 challenges

--- day-11/monkey_throwing.py
+++ day-11/monkey_throwing.py
@@ -9,15 +9,6 @@ class PART(Enum):
     TWO = 1
 
 
-@dataclass
-class Monkey:
-    items: List[int]
-    operation: str = ""
-    test_divis: int = 0
-    if_true: int = 0
-    if_false: int = 0
-    interactions: int = 0
-
 def str_to_int(x):
     return int(x)
 
@@ -27,24 +18,26 @@ def get_monkey_list(puzzle_input):
     for line in puzzle_input:
         line = line.strip()
         if "Monkey" in line:
-            monkey = Monkey(items=[])
+            monkey = []
         elif "Starting items:" in line:
             line = line.replace("Starting items: ", "")
             line = line.split(", ")
-            monkey.items = list(map(str_to_int, line))
+            line = list(map(str_to_int, line))
+            monkey.append(line)
         elif "Operation" in line:
             line = line.replace("Operation: new = ", "")
             line = eval("lambda old:" + line)
-            monkey.operation = line
+            monkey.append(line)
         elif "Test" in line:
             line = line.replace("Test: divisible by ", "")
-            monkey.test_divis = int(line)
+            monkey.append(int(line))
         elif "If true" in line:
             line = line.replace("If true: throw to monkey ", "")
-            monkey.if_true = int(line)
+            monkey.append(int(line))
         elif "If false" in line:
             line = line.replace("If false: throw to monkey ", "")
-            monkey.if_false = int(line)
+            monkey.append(int(line))
+            monkey.append(0)
             monkey_list.append(monkey)
     return monkey_list
 
@@ -57,23 +50,28 @@ def get_answer(part: PART):
         rounds = 20
     if part is PART.TWO:
         rounds = 10000
+        super_modulo = 1
+        for monkey in monkey_list:
+            super_modulo *= monkey[2]
 
     for i in range(rounds):
-        print(i)
         for monkey in monkey_list:
-            for item in monkey.items:
-                item_score = monkey.operation(item)
+            for item in monkey[0]:
+                item_score = monkey[1](item)
                 if part is PART.ONE:
-                    item_score = floor(item_score/3)
-                if item_score % monkey.test_divis == 0:
-                    monkey_list[monkey.if_true].items.append(item_score)
+                    item_score = floor(item_score / 3)
+                elif part is PART.TWO:
+                    item_score = item_score % super_modulo
+                if item_score % monkey[2] == 0:
+                    monkey_list[monkey[3]][0].append(item_score)
                 else:
-                    monkey_list[monkey.if_false].items.append(item_score)
-                monkey.interactions += 1
-            monkey.items = []
+                    monkey_list[monkey[4]][0].append(item_score)
+                monkey[5] += 1
+            monkey[0] = []
 
-    sorted_list = sorted(monkey_list, key=lambda x: x.interactions)
-    return sorted_list[-1].interactions * sorted_list[-2].interactions
+    sorted_list = sorted(monkey_list, key=lambda x: x[5])
+    return sorted_list[-1][5] * sorted_list[-2][5]
 
-print(get_answer(PART.ONE))
-print(get_answer(PART.TWO))
+
+print("part one answer:", get_answer(PART.ONE))
+print("part two answer:", get_answer(PART.TWO))
